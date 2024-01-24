728a932ca27f0ae4fc130d1bf6cd9869896cf4bb
harshpa324 <harshpa324@gmail.com>
2024-01-24 18:03:23 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <MaxWidthWrapper>
       <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
       <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-            Your marketplace for high-quality{' '}
+            Your marketplace for high-quality and very good{' '}
             <span className='text-blue-600'>
               digital assets
             </span>
